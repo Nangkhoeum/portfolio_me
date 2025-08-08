766a92d6d0bc896e5e7eff5d6e41a7d7ef14571e
Nangkhoeum <nangkhoeum.champhai@student.passereellesnumeriques.org>
2025-08-08 14:39:38 +0700

    put gmail

--- src/App.vue
+++ src/App.vue
@@ -161,7 +161,7 @@
                         d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                     </svg>
                   </div>
-                  <span class="text-gray-400 text-sm">contact@example.com</span>
+                  <span class="text-gray-400 text-sm">cpnkhoeumvd030005@gmail.com</span>
                 </div>
                 <div class="flex items-center space-x-3">
                   <div class="w-8 h-8 bg-green-500/20 rounded-lg flex items-center justify-center">
@@ -170,7 +170,7 @@
                         d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                     </svg>
                   </div>
-                  <span class="text-gray-400 text-sm">+1 (555) 123-4567</span>
+                  <span class="text-gray-400 text-sm">088 468 3201</span>
                 </div>
                 <div class="flex items-center space-x-3">
                   <div class="w-8 h-8 bg-purple-500/20 rounded-lg flex items-center justify-center">
